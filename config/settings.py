9a1ee621747e9e4242170c9ca2b33ad86564930b
Alexander Averin <alexaverin201156@gmail.com>
2025-07-15 16:14:01 +0300

    alpha rollout

--- config/settings.py
+++ config/settings.py
@@ -52,12 +52,12 @@ CELERY_TASK_TRACK_STARTED = True
 # Максимальное время на выполнение задачи
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
-# CELERY_BEAT_SCHEDULE = {
-#     'check_inactive_users': {
-#         'task': 'users.tasks.check_inactive_users',  # Путь к задаче
-#         'schedule': timedelta(days=1),  # Расписание выполнения задачи
-#     },
-# }
+CELERY_BEAT_SCHEDULE = {
+    'task-name': {
+        'task': 'users.tasks.check_inactive_users',  # Путь к задаче
+        'schedule': timedelta(days=1),  # Расписание выполнения задачи
+    },
+}
 
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_HTTPONLY = False
