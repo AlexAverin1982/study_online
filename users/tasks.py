dedd09687a0caf72d2abc8a862c82a5d22d0c8fd
Alexander Averin <alexaverin201156@gmail.com>
2025-07-15 16:14:01 +0300

    alpha rollout

--- users/tasks.py
+++ users/tasks.py
@@ -1,13 +1,12 @@
 import datetime as dt
 from celery import shared_task
-from django.shortcuts import get_object_or_404
 from users.models import CustomUser
 
+
 @shared_task
-def check_inactive_users(pk, model):
-    print('*'*100)
-    user = get_object_or_404(CustomUser, id=pk)
+def check_inactive_users():
     now = dt.datetime.now(dt.timezone.utc)
-    if (now - user.last_login).total_seconds() > 3600 * 24 * 20:
-        user.is_active = False
-        user.save()
+    for user in CustomUser.objects.all():
+        if user.is_active and ((now - user.last_login).total_seconds() > 3600 * 24 * 30):
+            user.is_active = False
+            user.save()
